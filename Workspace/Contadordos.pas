7caf4426dac913bb5e8078a76e16f9caf3095d30
patricio1204 <patososa1204@gmail.com>
2025-08-05 11:38:41 -0300

    Add files via upload

---
+++ Workspace/Contadordos.pas
@@ -0,0 +1,11 @@
+program contadordos;
+var i : integer;
+
+begin
+   i := 1;
+   while i <> 8 do
+    begin
+      i := succ(i);
+    end;
+    writeLn(i)       
+end.
---
+++ Workspace/Helloworld.pas
@@ -0,0 +1,5 @@
+program Helloworld;
+
+begin
+    writeLn('Helloworld');
+end.
